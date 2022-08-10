d9de0645a1bef45a4d77ab25f92b9aa2a3a261f1
Kensuke Nagae <kyanny@github.com>
2022-08-10 11:58:55 +0000

    Build an app

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'ipaddr'
+
+helpers do
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+end
 
 get '/' do
-  "Hello World!"
+    ip = params['ip'] || '192.168.0.1'
+    cidr = params['cidr'] || '192.168.0.0/24'
+    begin
+        result = IPAddr.new(cidr).include?(IPAddr.new(ip))
+    rescue IPAddr::InvalidAddressError => error
+        p error.inspect
+    end
+    erb :index, locals: { ip: ip, cidr: cidr, result: result, error: error }
 end
